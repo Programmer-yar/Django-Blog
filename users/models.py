c9359b80cd906b91166cbdb482c4dc825e55f630
Yar <62334099+Programmer-yar@users.noreply.github.com>
2020-07-29 12:42:53 +0500

    Add files via upload

---
+++ users/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image   #from pillow library import image
+
+class Profile(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE) #One to one relationship with(name of model)
+	"""CASCADE means if we delete the the user then also delete the profile but not vice versa"""
+	image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+
+
+	def __str__(self):
+		#Dipslays specific itmes with customization
+		return f'{self.user.username} Profile'
+
+	def save(self, *args, **kwargs):
+		#This method runs after our form is saved
+
+		#First run the save method of our parent class
+		super(Profile, self).save(*args, **kwargs)
+		"""When you are overriding model's save method in Django,
+		you should also pass *args and **kwargs to overridden method. """
+
+		img = Image.open(self.image.path)
+
+		if img.height > 300 or img.width > 300:
+			output_size = (300, 300)  #tuple
+			img.thumbnail(output_size)
+
+			img.save(self.image.path)  #override the existing image in the same path
+
+
+
+
+
+
+
+
